a0a17f29ab4d9c105d961b3d993e2bd8dbdfcee4
jpgvandijk <jeroen-van-dijk@hotmail.com>
2020-05-20 16:21:48 +0200

    Start of UART implementation

---
+++ inc/uart.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "pin.h"
+#include "clock.h"
+
+namespace System {
+
+	/************************************
+	* UART Base Implementation			*
+	************************************/
+	class UART {
+
+	public:
+		typedef enum {
+			char_5b = 0,
+			char_6b = 1,
+			char_7b = 2,
+			char_8b = 3
+		} CharacterLength;
+
+		typedef enum {
+			stop_1 = 0,
+			stop_1_5 = 1,
+			stop_2 = 1
+		} StopBits;
+
+		typedef enum {
+			none = 0,
+			odd = 1,
+			even = 3,
+			high = 5,
+			low = 7,
+		} Parity;
+
+	private:
+		LPC_UART_TypeDef * _lpc_uart;
+
+	protected:
+		UART (uint32_t instance);
+		void initialize (uint32_t pin_txd, Pin::Function function, uint32_t peripheral_frequency, uint32_t baudrate, uint8_t mode);
+		void handle (void);
+		uint8_t mode (CharacterLength character_length, StopBits stop_bits, Parity parity, bool enable_break_control);
+		void setBaudrate (uint32_t peripheral_frequency, uint32_t baudrate);
+
+	public:
+		void write (uint8_t * tx_buffer, uint8_t tx_length);
+	};
+
+	/************************************
+	* UART0 Singleton					*
+	************************************/
+	class UART0 : public UART
+	{
+	private:
+		UART0 (void);
+		UART0 (UART0 const&) = delete;
+		void operator= (UART0 const&) = delete;
+		static void handleInterrupt (void);
+
+	public:
+		static UART0 & instance (void);
+		void initialize (System::Clock::PeripheralClockSpeed clock = System::Clock::PeripheralClockSpeed::cpu_divide_by_4,
+						uint32_t baudrate = 9600,
+						UART::CharacterLength character_length = UART::CharacterLength::char_8b,
+						UART::StopBits stop_bits = UART::StopBits::stop_1,
+						UART::Parity parity = UART::Parity::none,
+						bool enable_break_control = false);
+	};
+
+	/************************************
+	* UART1 Singleton					*
+	************************************/
+	class UART1 : public UART
+	{
+	private:
+		UART1 (void);
+		UART1 (UART1 const&) = delete;
+		void operator= (UART1 const&) = delete;
+		static void handleInterrupt (void);
+	public:
+		static UART1 & instance (void);
+		void initialize (System::Clock::PeripheralClockSpeed clock = System::Clock::PeripheralClockSpeed::cpu_divide_by_4);
+	};
+
+	/************************************
+	* UART2 Singleton					*
+	************************************/
+	class UART2 : public UART
+	{
+	private:
+		UART2 (void);
+		UART2 (UART2 const&) = delete;
+		void operator= (UART2 const&) = delete;
+		static void handleInterrupt (void);
+	public:
+		static UART2 & instance (void);
+		void initialize (System::Clock::PeripheralClockSpeed clock = System::Clock::PeripheralClockSpeed::cpu_divide_by_4);
+	};
+
+	/************************************
+	* UART3 Singleton					*
+	************************************/
+	class UART3 : public UART
+	{
+	private:
+		UART3 (void);
+		UART3 (UART3 const&) = delete;
+		void operator= (UART3 const&) = delete;
+		static void handleInterrupt (void);
+	public:
+		static UART3 & instance (void);
+		void initialize (System::Clock::PeripheralClockSpeed clock = System::Clock::PeripheralClockSpeed::cpu_divide_by_4);
+	};
+}
---
+++ src/uart.cpp
@@ -0,0 +1,220 @@
+// Includes
+#include "LPC17xx.h"
+#include "uart.h"
+#include "clock.h"
+#include "pin.h"
+#include "interrupt.h"
+
+// Namespaces
+using namespace System;
+
+// TODO: register for CPU clock frequency updates!
+
+/************************************
+* UART Interrupt Handlers			*
+************************************/
+
+namespace {
+	void (*handleInterruptPointer[4]) (void) = {nullptr, nullptr, nullptr, nullptr};
+}
+
+extern "C" {
+
+	void UART0_IRQHandler (void) {
+		if (handleInterruptPointer[0] != nullptr) {
+			handleInterruptPointer[0]();
+		}
+	}
+
+	void UART1_IRQHandler (void) {
+		if (handleInterruptPointer[1] != nullptr) {
+			handleInterruptPointer[1]();
+		}
+	}
+
+	void UART2_IRQHandler (void) {
+		if (handleInterruptPointer[2] != nullptr) {
+			handleInterruptPointer[2]();
+		}
+	}
+
+	void UART3_IRQHandler (void) {
+		if (handleInterruptPointer[3] != nullptr) {
+			handleInterruptPointer[3]();
+		}
+	}
+}
+
+namespace System {
+
+	/************************************
+	* UART Base Implementation			*
+	************************************/
+
+	UART::UART (uint32_t instance) {
+		if (instance == 0) {
+			_lpc_uart = (LPC_UART_TypeDef*) LPC_UART0;
+		} else if (instance == 1) {
+			_lpc_uart = (LPC_UART_TypeDef*) LPC_UART1;
+		} else if (instance == 2) {
+			_lpc_uart = LPC_UART2;
+		} else { // (instance == 3)
+			_lpc_uart = LPC_UART3;
+		}
+	}
+
+	void UART::initialize (uint32_t pin_txd, Pin::Function function, uint32_t peripheral_frequency, uint32_t baudrate, uint8_t mode) {
+
+		// Init TXD pin
+		Pin::setFunction(pin_txd, function);
+		Pin::setPullMode(pin_txd, Pin::PullMode::no_pull);
+		Pin::setOpenDrain(pin_txd, false);
+
+		// Init SCL pin
+		uint32_t pin_rxd = pin_txd + 1;
+		Pin::setFunction(pin_rxd, function);
+		Pin::setPullMode(pin_rxd, Pin::PullMode::no_pull);
+		Pin::setOpenDrain(pin_rxd, false);
+
+		// Set the desired baudrate
+		setBaudrate(peripheral_frequency, baudrate);
+
+		// Enable the UART interface
+		_lpc_uart->LCR = mode;
+		_lpc_uart->FCR = (1 << 0) | (1 << 1) | (1 << 2);
+		_lpc_uart->IER = (1 << 0) | (1 << 1);
+	}
+
+	void UART::handle (void) {
+
+	}
+
+	uint8_t UART::mode (UART::CharacterLength character_length, UART::StopBits stop_bits, UART::Parity parity, bool enable_break_control) {
+		uint8_t mode = (((uint8_t) character_length) << 0) | (((uint8_t) stop_bits) << 2) | (((uint8_t) parity) << 3);
+		if (enable_break_control) {
+			mode |= (1 << 6);
+		}
+		return mode;
+	}
+
+	void UART::setBaudrate (uint32_t peripheral_frequency, uint32_t baudrate) {
+
+		/********************************************************
+		* Baudrate Calculation									*
+		* q = (256 * UnDLM + UnDLL) * (MULVAL + DIVADDVAL)		*
+		********************************************************/
+		baudrate = 16 * baudrate;
+
+		// Try all fractional multiplier settings to determine the closest match
+		uint32_t min_error = 0xFFFFFFFF;
+		uint32_t min_error_multiplier = 1;
+		for (uint32_t multiplier = 1; multiplier < 16; multiplier++) {
+			uint32_t multiplied_frequency = peripheral_frequency * multiplier;
+			uint32_t q = (multiplied_frequency + (baudrate >> 1)) / baudrate;
+
+			q = q * baudrate;
+			uint32_t error;
+			if (q > multiplied_frequency) {
+				error = q - multiplied_frequency;
+			} else {
+				error = multiplied_frequency - q;
+			}
+
+			if (error < min_error) {
+				min_error = error;
+				min_error_multiplier = multiplier;
+				if (error == 0) {
+					break;
+				}
+			}
+		}
+		uint32_t multiplied_frequency = peripheral_frequency * min_error_multiplier;
+		uint32_t q = (multiplied_frequency + (baudrate >> 1)) / baudrate;
+
+		// Now find the best divider setting
+		min_error = 0xFFFFFFFF;
+		uint32_t min_error_divider = 0;
+		for (uint32_t divider = 0; divider < min_error_multiplier; divider++) {
+			uint32_t sum = min_error_multiplier + divider;
+			uint32_t dl = (q + (sum >> 1)) / sum;
+
+			sum = sum * dl;
+			uint32_t error;
+			if (sum > q) {
+				error = sum - q;
+			} else {
+				error = q - sum;
+			}
+
+			if (error < min_error) {
+				min_error = error;
+				min_error_divider = divider;
+				if (error == 0) {
+					break;
+				}
+			}
+		}
+
+		// Fraction not required?
+		_lpc_uart->LCR |= (1 << 7);
+		if (min_error_divider == 0) {
+			uint32_t dl = (peripheral_frequency + (baudrate >> 1)) / baudrate;
+			_lpc_uart->FDR = (1 << 4) | (0 << 0);
+			_lpc_uart->DLL = (dl >> 0) & 0xFF;
+			_lpc_uart->DLM = (dl >> 8) & 0xFF;
+		} else {
+			_lpc_uart->FDR = (min_error_multiplier << 4) | (min_error_divider << 0);
+			uint32_t sum = min_error_multiplier + min_error_divider;
+			uint32_t dl = (q + (sum >> 1)) / sum;
+			_lpc_uart->DLL = (dl >> 0) & 0xFF;
+			_lpc_uart->DLM = (dl >> 8) & 0xFF;
+		}
+		_lpc_uart->LCR &= ~(1 << 7);
+	}
+
+	void UART::write (uint8_t * tx_buffer, uint8_t tx_length) {
+
+	}
+}
+
+/************************************
+* UART0 Singleton					*
+************************************/
+
+UART0 & UART0::instance (void) {
+	static UART0 uart;
+	return uart;
+}
+
+UART0::UART0 (void) : UART(0) {
+	handleInterruptPointer[0] = handleInterrupt;
+}
+
+void UART0::handleInterrupt (void) {
+	instance().handle();
+}
+
+void UART0::initialize (Clock::PeripheralClockSpeed clock, uint32_t baudrate, UART::CharacterLength character_length, UART::StopBits stop_bits, UART::Parity parity, bool enable_break_control) {
+	Clock::enablePeripheral(Clock::PeripheralPower::uart_0_power);
+	Clock::setPeripheralClock(Clock::PeripheralClock::uart_0_clock, clock);
+	uint32_t frequency = Clock::getPeripheralClockFrequency(Clock::PeripheralClock::uart_0_clock);
+
+	uint32_t pin_txd = PIN(0, 2);
+	uint8_t mode = UART::mode(character_length, stop_bits, parity, enable_break_control);
+	UART::initialize(pin_txd, Pin::Function::alternate_1, frequency, baudrate, mode);
+	System::Interrupt::enable(UART0_IRQn);
+}
+
+
+
+// alt 2 or alt 3 or alt 3
+// TXD3: P0.0 or 0.25 or 4.28
+// RXD3: P0.1 or 0.26 or 4.29
+
+// alt 1 or alt 2
+// TXD2: P0.10 or 2.8
+// RXD2: P0.11 or 2.9
+
+// alt 1 or alt 2
+// TXD1: P0.15 or 2.0
+// RXD1: P0.16 or 2.1
